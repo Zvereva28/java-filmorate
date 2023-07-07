f52699ae7f4150bb93be23d69ebda691eb8964c2
VTB4053680@region.vtb.ru <zverevaeg@vtb.ru>
2023-07-07 17:11:10 +0300

    fix: убрала пустые строки

--- src/main/java/ru/yandex/practicum/filmorate/model/enums/RatingFilms.java
+++ src/main/java/ru/yandex/practicum/filmorate/model/enums/RatingFilms.java
@@ -4,7 +4,6 @@ import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import ru.yandex.practicum.filmorate.exception.MpaNotFoundException;
-
 import java.util.Arrays;
 
 @AllArgsConstructor
