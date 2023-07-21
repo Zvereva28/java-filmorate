5b907a4fc1b571e486fe476f584ef0f699d1ff9b
altpa <altpass@gmil.com>
2023-07-21 11:41:40 +0300

    add-recommendation for PR/Checkstyle

--- src/test/java/ru/yandex/practicum/filmorate/controller/UserControllerRecommendationsTest.java
+++ src/test/java/ru/yandex/practicum/filmorate/controller/UserControllerRecommendationsTest.java
@@ -5,7 +5,6 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.test.context.jdbc.Sql;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
